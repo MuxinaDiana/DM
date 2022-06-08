fcde81def3c6a1fc94850b0b25a8713dac7affb9
user12412 <muxinad01@mail.ru>
2022-06-08 11:09:20 +0300

    Module-1
    Molule-1

---
+++ bibliotekamux/src/main/java/com/example/bibliotekamux/ColorPicker.java
@@ -0,0 +1,23 @@
+package com.example.bibliotekamux;
+import android.content.Context;
+
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ColorPicker extends View {
+
+
+    public ColorPicker(Context context){
+        super(context);//Получение activity
+
+    }
+    public ColorPicker(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);//Получение activity иатрибута
+
+    }
+}
+
+
+
